0a8a4fa6d9fca4f97492f57f5e15a83f185a2864
nktshklv-dev <nikitashakalov@yandex.by>
2024-12-10 20:58:17 +0300

    created DynamicArray

---
+++ DynamicArray.h
@@ -0,0 +1,172 @@
+#ifndef DYNAMICARRAY_H
+#define DYNAMICARRAY_H
+
+#include <__filesystem/recursive_directory_iterator.h>
+
+#include "ISequence.h"
+
+template <class T>
+class DynamicArray: public ISequence<T> {
+private:
+    T* data;
+    int size;
+
+    void Resize(int newSize) {
+        T* newData = new T[newSize];
+
+        int minSize = (newSize < size) ? newSize : size;
+
+        for (int i = 0; i < newSize; ++i) {
+            newData[i] = data[i];
+        }
+        delete[] data;
+        data = newData;
+        size = newSize;
+    }
+public:
+    class DynamicArrayIterator: public  ISequence<T>::Iterator {
+    private:
+        T* current;
+    public:
+        DynamicArrayIterator(T* current) : current(current) { }
+        bool operator ==(const typename ISequence<T>::Iterator& other) const override {
+            const DynamicArrayIterator* otherIterator = dynamic_cast<const DynamicArrayIterator*>(other);
+            return otherIterator && current == otherIterator->current;
+        }
+
+        bool operator !=(const typename ISequence<T>::Iterator& other) const override{
+            return !(*this == other);
+        }
+
+        T& operator * () override {
+            return *current;
+        }
+        typename ISequence<T>::Iterator& operator ++ () override {
+            current++;
+
+            return *this;
+        }
+    };
+    typename ISequence<T>::Iterator* ToBegin() override {
+        return new DynamicArrayIterator(data);
+    }
+
+    typename ISequence<T>::Iterator* ToEnd() override {
+        return new DynamicArrayIterator(data + size);
+    }
+
+    DynamicArray(T* items, int size) {
+        this->size = size;
+        data = new T[size];
+
+        for(int i = 0; i < size; ++i) {
+            Set(i, items[i]);
+        }
+    }
+    DynamicArray(int size = 0) {
+        this->size = size;
+        data = new T[size];
+    }
+    DynamicArray(DynamicArray<T>& dynamicArray) {
+        size = dynamicArray.size;
+        data = new T[size];
+
+        for (int i =0; i < size; ++i) {
+            Set(i, dynamicArray.data[i]);
+        }
+    }
+
+    ~DynamicArray() {
+        delete[] data;
+    }
+
+    T& operator [](int index) {
+        return data[index];
+    }
+
+    T& GetFirstElement() override {
+        return GetElement(0);
+    }
+
+    T& GetLastElement() override {
+        return GetElement(size - 1);
+    }
+
+    T& GetElement(int index) override {
+        return data[index];
+    }
+
+    void Swap(T a, T b) override {
+        T temp = a;
+        a = b;
+        b = temp;
+    }
+    void Set(int index, T value) override {
+        data[index] = value;
+    }
+
+    DynamicArray<T> GetSubSequence(int startIndex, int endIndex) override {
+        int length;
+
+        if (endIndex > size) {
+            length = size - startIndex;
+        }
+        else {
+            length = endIndex - startIndex + 1;
+
+            if (startIndex == 0) {
+                length -= 1;
+            }
+        }
+
+        T* items = new T[length];
+
+        for (int i = 0; i < length; ++i) {
+            items[i] = GetElement(startIndex + i);
+        }
+
+        return new DynamicArray<T>(items, length);
+    }
+
+    int GetLength() override {
+        return size;
+    }
+
+    void Append(T data) override {
+        InsertAt(data, size);
+    }
+
+    void Append( T data, int dataSize) override {
+        int oldSize = size;
+
+        Resize(size + dataSize);
+
+        for(int i = oldSize; i < oldSize + dataSize; i++) {
+            Set(i, data[i - oldSize]);
+        }
+    }
+
+    void Prepend(T data) override {
+        InsertAt(data, 0);
+    }
+
+    void InsertAt(T data, int index) override {
+        Resize(size + 1);
+
+        for (int i = size - 1; i > index; --i);
+        {
+            Set(i, GetElement(i - 1));
+        }
+
+        Set(index, data);
+    }
+
+    void Union(ISequence<T>* dynamicArray) override {
+        for (int i = 0; i < dynamicArray->GetLength(); ++i) {
+            Append(dynamicArray->GetElement(i));
+        }
+    }
+};
+
+
+#endif //DYNAMICARRAY_H
