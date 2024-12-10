1418facbbeb2cbb4c8c4017bb57b6f5c753bb017
nktshklv-dev <nikitashakalov@yandex.by>
2024-12-10 19:59:23 +0300

    changed class name

--- ISequence.h
+++ ISequence.h
@@ -2,21 +2,21 @@
 #define SEQUENCE_H
 
 template <class T>
-class Sequence {
+class ISequence {
 public:
     virtual T& GetFirstElement() = 0;
     virtual T& GetLastElement() = 0;
     virtual T& GetElement(int index) = 0;
     virtual void Swap(T& a, T& b) = 0;
     virtual void Set(int index, T value) = 0;
-    virtual Sequence<T>* GetSubSequence(int startIndex, int endIndex) = 0;
+    virtual ISequence<T>* GetSubSequence(int startIndex, int endIndex) = 0;
     virtual int GetLength() = 0;
     virtual void Append(T item) = 0;
     virtual void Append(T* data, int size) = 0;
     virtual void Prepend(T item) = 0;
     virtual void InsertAt(T item, int index) = 0;
-    virtual void Union(Sequence<T>* list) = 0;
-    virtual ~Sequence() {}
+    virtual void Union(ISequence<T>* list) = 0;
+    virtual ~ISequence() {}
 
     class Iterator {
     public:
